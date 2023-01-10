f43525302fe953ce10e57699f91ccb146d2ae277
Carlos Espinoza <soncarlos1982@gmail.com>
2023-01-09 20:46:30 -0600

    Challenges Finished

---
+++ FUNDA04 JS/ChallengesW07/090123/Boolean.js
@@ -0,0 +1,6 @@
+function nand(a,b){
+    let and= a && b;
+    return !and;
+    }
+    let x =nand(true, true); 
+     console.log(x);
---
+++ FUNDA04 JS/ChallengesW07/ToFahrenheit.js
@@ -0,0 +1,4 @@
+function toFahrenheit(celsius){
+    return (celsius*1.8) + 32;
+    }
+    console.log(toFahrenheit(0));
