78dc59314292686489b37c135f822d49aeb5c412
Carlos Espinoza <soncarlos1982@gmail.com>
2023-02-08 22:14:30 -0600

    Updates

--- FUNDA04 JS/ChallengesW08/190123/Encrypt_this.js
+++ FUNDA04 JS/ChallengesW08/190123/Encrypt_this.js
@@ -1,24 +1,22 @@
-str1="Hello"//=== "72olle"
-str2="good"// === "103doo"
+str1="A"//=== "72olle"
+str2="A wise old owl lived in an oak"// === "103doo"
 str3="hello world"// === "104olle 119drlo"
-const encrypt=((word)=>{   
-    let charCode=word.charCodeAt(0);
-    let secondchar=word[1];
-    console.log(word)
-    word=word.replace(word[0], charCode);
-    console.log(word);
-    console.log(secondchar)
-    word=word.replace(secondchar,word[word.length-1]);
-    console.log(word)
-    word=word.replace(/\w$/, secondchar);
-    return word;
-});
+const encrypt=((word)=>{
+  let charCode=word.charCodeAt(0);  
+  if(word.length===1) return word=charCode;
+      let secondchar=word[1];    
+      word=word.replace(word[0], charCode);       
+      word=word.replace(secondchar,word[word.length-1]);    
+      word=word.replace(/\w$/, secondchar);
+      return word;     
+    });
+
 var encryptThis = function(text) {
   let arr=text.split(" ");
   let result="";
   arr.forEach((word) =>{
     result= result +' '+ encrypt(word);
   });
-  return result;  
+  return result.trim();  
 }
-console.log(encryptThis(str3));
+console.log(encryptThis(str));
--- ProjectCalcuCoreCode/app.js
+++ ProjectCalcuCoreCode/app.js
@@ -1,17 +1,99 @@
+//========================= MEMORY
+let memory=[];
+let activeOperation= null;
+let prevOperation= null;
+/*FEATURES
+* 1-> FONT DISPLAY BASE IN RESULT LENGTH.
+* 2-> COPY PERCENT BEHAVIOR
+* 3-> HISTORY
+* 4-> BACKSPACE (DELETE SINGLE DIGIT)
+*/
+
 //========================= WORK WITH DISPLAY
 const display= document.querySelector('#display');
 
 //========================= WORK WITH AC
 const ac= document.querySelector('#ac');
+ac.addEventListener('click', ()=>{
+display.innerHTML='0'; ac.innerHTML='AC';
+memory=[];
+activeOperation= null;
+prevOperation= null;
+});
 
 //========================= WORK WITH SIG
 const sig= document.querySelector('#sig');
+sig.addEventListener('click', ()=>{
+let currentDisplay= display.innerHTML;
+let newdisplay= `${Number(currentDisplay*-1)}`; 
+if(newdisplay ==='0'){
+newdisplay= '-0';
+}
+display.innerHTML= newdisplay;
+})
 
 //========================= WORK WITH PERCENT
 const percent= document.querySelector('#percent');
 
 //========================= WORK WITH DECIMAL
 const decimal= document.querySelector('#dot');
+const decimalHandler= ()=>{
+const currenDisplay= display.innerHTML;
+if(currenDisplay.includes('.')) return;
+display.innerHTML=`${currenDisplay}.`;
+
+
+}
+decimal.addEventListener('click', ()=> decimalHandler());
+
+//========================== EQUAL
+const equal= document.querySelector('#equal');
+const equalHandler = () =>{
+const operation= `${memory.join(' ')} ${Number(display.innerHTML)}`
+display.innerHTML= `${Math.round(eval(operation))}`;
+memory=[];
+activeOperation=null;
+prevOperation=null;
+}
+equal.addEventListener('click',()=> equalHandler())
+
+//========================== OPERATORS
+const sum= document.querySelector('#sum');
+const sub= document.querySelector('#sub');
+const mul= document.querySelector('#mul');
+const div= document.querySelector('#div');
+const operators=[
+{el:sum, op:'+'},
+{el:sub, op:'-'},
+{el:mul, op:'*'},
+{el:div, op:'/'},
+];
+
+const selectedOperator = (el) =>{
+el.style.backgroundColor='#ffffff';
+el.style.color='#ee6c4d';  
+}
+const unselectedOperator= (el)=>{
+el.style.backgroundColor='#ee6c4d';
+el.style.color='#ffffff';  
+}
+
+const operatorHandler = (op,el)=>{
+selectedOperator(el);
+const currenDisplay= display.innerHTML;
+if(memory.length===0){
+  memory.push(currenDisplay);
+}
+if(memory.length > 1){
+  equalHandler();
+  memory.push(Number(display.innerHTML));
+}
+memory.push(op);
+activeOperation=el;
+prevOperation=el;
+}
+operators.forEach((oper)=>
+oper.el.addEventListener('click', ()=> operatorHandler(oper.op, oper.el)));
 
 //========================= WORK WITH NUMBERS
 const zero= document.querySelector('#zero');
@@ -28,13 +110,18 @@ const nine= document.querySelector('#nine');
 const numbers=[zero,one, two, three,four, five, six, seven, eight, nine];
 
 const numberHandler=(n)=>{
-    const currenDisplay= display.innerHTML;
- let newdisplay=`${currenDisplay}${n}`;
- if(currenDisplay==='0'){
-    newdisplay= n;
- }
- display.innerHTML=`${Number(newdisplay)}`;
-
-};
-
-numbers.forEach((n, i) => n.addEventListener('click', () => numberHandler(i)));
+if(prevOperation!==null){
+  unselectedOperator(prevOperation);
+  display.innerHTML= '';
+  activeOperation= null;
+}
+ac.innerHTML='C'; 
+const currenDisplay= display.innerHTML;
+let newdisplay=`${currenDisplay}${n}`;
+if(currenDisplay==='0'){
+newdisplay= n;
+}
+display.innerHTML= `${Number(newdisplay)}`;
+}
+numbers.forEach((n, i) => n.addEventListener('click', () => numberHandler(i))
+);
