077ceb065fd3de2beb5352dabda445a6a696975a
Carlos Espinoza <soncarlos1982@gmail.com>
2023-02-22 20:12:02 -0600

    updates

--- FUNDA04JS/ChallengesW11/160223/ApiRestPractice/src/App.js
+++ FUNDA04JS/ChallengesW11/160223/ApiRestPractice/src/App.js
@@ -1,17 +1,17 @@
-const express = require('express')
+import express from 'express'
 const app = express();
 const port = 3852;
-const del = 5000;
 
-
-app.get('/', (req, res) => {
-
-    (setTimeout(() => {
-        res.send(`Dilate ${del / 1000} segundos en aparecer`);
-    }, del))
-
+app.get("/api/delay/:tiempo", (req, res) => {
+    const { tiempo } = req.params;
+    const mensaje = "Digite un numero mayor a 1000";
+    const time = () => {
+        (setTimeout(() => {
+            res.send(`Dilate ${tiempo / 1000} segundos en aparecer`);
+        }, tiempo));
+    }
+    (tiempo > 0) ? time() : res.send(mensaje);
 })
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
