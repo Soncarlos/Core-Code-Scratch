342c16b9d7b8a05c3174dcdb17146e65cabb6e69
Carlos Espinoza <soncarlos1982@gmail.com>
2023-02-23 22:17:22 -0600

    feat: Adding Init DB and Table

--- InfinnityGaunlet/Community-Tesseract/src/db/index.js
+++ InfinnityGaunlet/Community-Tesseract/src/db/index.js
@@ -1,7 +1,7 @@
 import { open } from "sqlite";
 import driver from "sqlite3";
 
-export async function getDb() {
+export async function getDbConnection() {
 
     try {
         const dbConnection = await open({
@@ -15,5 +15,21 @@ export async function getDb() {
 
     } catch (error) {
         console.error("There was an error trying to connect to the DBMS");
+    }
+}
+export async function initDB() {
+    try {
+        const db = await getDbConnection();
+        await db.exec(`CREATE TABLE IF NOT EXISTS todos(
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            description TEXT,
+            is_done INTEGER DEFAULT 0)`)
+
+        await db.close();
+    } catch (ex) {
+        console.error(
+            "There was an error, trying to init DB",
+            ex.error);
     }
 }
--- InfinnityGaunlet/Community-Tesseract/src/index.js
+++ InfinnityGaunlet/Community-Tesseract/src/index.js
@@ -1,3 +1,20 @@
-import { getDb } from "./db/index.js";
+import express from 'express'
+import cors from 'cors'
+import { initDB } from './db/index.js'
 
-getDb();
+const api = express();
+
+const apiPort = process.env["APP_ENV"] || 3000;
+
+api.use(express.json());
+api.use(express.urlencoded({ extends: false }))
+api.use(cors());
+
+
+api.listen(apiPort, () => {
+    console.log(`API RUNNING IN PORT : ${apiPort}`)
+    initDB().then(() => {
+        console.log('DB INITIALIZED')
+    });
+})
+
